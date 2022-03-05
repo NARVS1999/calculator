ee4c8aebe7357432f2892831f0ecce386f610f05
NARVS1999 <johnpaulnarvasa6@gmail.com>
2022-03-05 21:04:58 +0800

    refactoring and debugging some keycode error

--- script.js
+++ script.js
@@ -53,104 +53,254 @@ const buttonMinus = document.querySelector('.minus-sign');
 const buttonMultiply = document.querySelector('.multiply-sign');
 const buttonDivide = document.querySelector('.divide-sign');
 const buttonEqual = document.querySelector('.equal-sign');
-const buttonClear = document.querySelector('.clear');
+const buttonDelete = document.querySelector('.clear');
 const buttonDotted = document.querySelector('.dotted-sign');
 const buttonBack = document.querySelector('.back-space');
 
-function limitNumber() {
+let num1 = '';
+let num2 = '';
+let operation = '';
+let displayValue = '';
+let answer = '';
+let erroMessage = '';
+
+function buttonOne() {
     if(displayValue.length === 9) {
         return;
+    } else {
+        if(answer !== '' || erroMessage !== '') {
+            displayScreen.textContent = '';
+            answer = '';
+        }
+        displayScreen.textContent += '1';
+        displayValue += '1'; 
     }
 }
 
-function display() {
-    button1.addEventListener('click', () => {
-        if(displayValue.length === 9) {
-
-        } else {
-            if(answer !== '' || erroMessage !== '') {
-                displayScreen.textContent = '';
-                answer = '';
-            }
-            displayScreen.textContent += '1';
-            displayValue += '1'; 
-        }
-    });
-    button2.addEventListener('click', () => {
+function buttonTwo() {
+    if(displayValue.length === 9) {
+        return;
+    } else {
         if(answer !== '' || erroMessage !== '') {
             displayScreen.textContent = '';
             answer = '';
         }
         displayScreen.textContent += '2';
-        displayValue += '2';
-    });
-    button3.addEventListener('click', () => {
+        displayValue += '2'; 
+    }
+}
+
+function buttonThree() {
+    if(displayValue.length === 9) {
+        return;
+    } else {
         if(answer !== '' || erroMessage !== '') {
             displayScreen.textContent = '';
             answer = '';
         }
         displayScreen.textContent += '3';
-        displayValue += '3';
-    });
-    button4.addEventListener('click', () => {
+        displayValue += '3'; 
+    }
+}
+
+function buttonFour() {
+    if(displayValue.length === 9) {
+        return;
+    } else {
         if(answer !== '' || erroMessage !== '') {
             displayScreen.textContent = '';
             answer = '';
         }
         displayScreen.textContent += '4';
-        displayValue += '4';
-    });
-    button5.addEventListener('click', () => {
+        displayValue += '4'; 
+    }
+}
+
+function buttonFive() {
+    if(displayValue.length === 9) {
+        return;
+    } else {
         if(answer !== '' || erroMessage !== '') {
             displayScreen.textContent = '';
             answer = '';
         }
         displayScreen.textContent += '5';
-        displayValue += '5';
-    });
-    button6.addEventListener('click', () => {
+        displayValue += '5'; 
+    }
+}
+
+function buttonSix() {
+    if(displayValue.length === 9) {
+        return;
+    } else {
         if(answer !== '' || erroMessage !== '') {
             displayScreen.textContent = '';
             answer = '';
         }
         displayScreen.textContent += '6';
-        displayValue += '6';
-    });
-    button7.addEventListener('click', () => {
+        displayValue += '6'; 
+    }
+}
+
+function buttonSeven() {
+    if(displayValue.length === 9) {
+        return;
+    } else {
         if(answer !== '' || erroMessage !== '') {
             displayScreen.textContent = '';
             answer = '';
         }
         displayScreen.textContent += '7';
-        displayValue += '7';
-    });
-    button8.addEventListener('click', () => {
+        displayValue += '7'; 
+    }
+}
+
+function buttonEight() {
+    if(displayValue.length === 9) {
+        return;
+    } else {
         if(answer !== '' || erroMessage !== '') {
             displayScreen.textContent = '';
             answer = '';
         }
         displayScreen.textContent += '8';
-        displayValue += '8';
-    });
-    button9.addEventListener('click', () => {
+        displayValue += '8'; 
+    }
+}
+
+function buttonNine() {
+    if(displayValue.length === 9) {
+        return;
+    } else {
         if(answer !== '' || erroMessage !== '') {
             displayScreen.textContent = '';
             answer = '';
         }
         displayScreen.textContent += '9';
-        displayValue += '9';
-    });
-    button0.addEventListener('click', () => {
+        displayValue += '9'; 
+    }
+}
+
+function buttonZero() {
+    if(displayValue.length === 9) {
+        return;
+    } else {
         if(answer !== '' || erroMessage !== '') {
             displayScreen.textContent = '';
             answer = '';
         }
         displayScreen.textContent += '0';
-        displayValue += '0';
-    });
-    
+        displayValue += '0'; 
+    }
 }
-display();
+
+function plusOperation() {
+    if(num1 === '' && displayValue === '') {
+        return;
+    } else if(num1 === '') {
+        num1 = Number(displayValue);
+        operation = '+';
+        displayValue = '';
+        displayScreen.textContent = '';
+    } else {
+        calculate();
+        operation = '+';
+        displayValue = '';
+        num2 = '';
+    }    
+}
+
+function minusOperation() {
+    if(num1 === '' && displayValue === '') {
+        return;
+    } else if(num1 === '') {
+        num1 = Number(displayValue);
+        operation = '-';
+        displayValue = '';
+        displayScreen.textContent = '';
+    } else {
+        calculate();
+        operation = '-';
+        displayValue = '';
+        num2 = '';
+    }    
+}
+
+function multiplyOperation() {
+    if(num1 === '' && displayValue === '') {
+        return;
+    } else if(num1 === '') {
+        num1 = Number(displayValue);
+        operation = '*';
+        displayValue = '';
+        displayScreen.textContent = '';
+    } else {
+        calculate();
+        operation = '*';
+        displayValue = '';
+        num2 = '';
+    }    
+}
+
+function divideOperation() {
+    if(num1 === '' && displayValue === '') {
+        return;
+    } else if(num1 === '') {
+        num1 = Number(displayValue);
+        operation = '/';
+        displayValue = '';
+        displayScreen.textContent = '';
+    } else {
+        calculate();
+        operation = '/';
+        displayValue = '';
+        num2 = '';
+    }    
+}
+
+function equalOperation() {
+    if(operation === '') {
+        return;
+    }
+    calculate();
+    num2 = '';
+    num1 = '';
+    operation = '';
+    displayValue = '';
+}
+
+function deleteOperation() {
+    displayScreen.textContent = '';
+    num1 = '';
+    num2 = '';
+    operation = '';
+    displayValue = '';
+    answer = '';
+}
+
+function backOperation() {
+    if(displayValue === '') {
+        return;
+    } else if(displayValue.length === 1) {
+        displayScreen.textContent = '';
+        displayValue = '';
+    } else {
+        let temp = displayValue;
+        temp = temp.substring(0, temp.length-1);
+        displayScreen.textContent = temp;
+        displayValue = temp;
+    }
+}
+
+function dottedOperation() {
+    for(let i = 0; i < displayValue.length; i++) {
+        if(displayValue[i] === '.') {
+            return;
+        }
+    }
+    displayScreen.textContent += ".";
+    displayValue += '.';
+}
 
 function calculate() {
     num2 = Number(displayValue);
@@ -169,287 +319,64 @@ function calculate() {
     }
 }
 
-let num1 = '';
-let num2 = '';
-let operation = '';
-let displayValue = '';
-let answer = '';
-let erroMessage = '';
-
-buttonPlus.addEventListener('click', () => {
-    if(num1 === '' && displayValue === '') {
-        return;
-    } else if(num1 === '') {
-        num1 = Number(displayValue);
-        operation = '+';
-        displayValue = '';
-        displayScreen.textContent = '';
-    } else {
-        calculate();
-        operation = '+';
-        displayValue = '';
-        num2 = '';
-    }    
-});
-
-buttonMinus.addEventListener('click', () => {
-    if(num1 === '' && displayValue === '') {
-        return;
-    } else if(num1 === '') {
-        num1 = Number(displayValue);
-        operation = '-';
-        displayValue = '';
-        displayScreen.textContent = '';
-    } else {
-        calculate();
-        operation = '-';
-        displayValue = '';
-        num2 = '';
-    }    
-});
-
-buttonMultiply.addEventListener('click', () => {
-    if(num1 === '' && displayValue === '') {
-        return;
-    } else if(num1 === '') {
-        num1 = Number(displayValue);
-        operation = '*';
-        displayValue = '';
-        displayScreen.textContent = '';
-    } else {
-        calculate();
-        operation = '*';
-        displayValue = '';
-        num2 = '';
-    }    
-});
-
-buttonDivide.addEventListener('click', () => {
-    if(num1 === '' && displayValue === '') {
-        return;
-    } else if(num1 === '') {
-        num1 = Number(displayValue);
-        operation = '/';
-        displayValue = '';
-        displayScreen.textContent = '';
-    } else {
-        calculate();
-        operation = '/';
-        displayValue = '';
-        num2 = '';
-    }    
-});
-
-buttonEqual.addEventListener('click', () => {
-    if(operation === '') {
-        return;
-    }
-    calculate();
-    num2 = '';
-    num1 = '';
-    operation = '';
-    displayValue = '';
-});
-// delete keyboard support
-buttonClear.addEventListener('click', () => {
-    displayScreen.textContent = '';
-    num1 = '';
-    num2 = '';
-    operation = '';
-    displayValue = '';
-    answer = '';
-});
-
-buttonDotted.addEventListener('click', () => {
-    for(let i = 0; i < displayValue.length; i++) {
-        if(displayValue[i] === '.') {
-            return;
-        }
-    }
-    displayScreen.textContent += ".";
-    displayValue += '.';
-})
-
-buttonBack.addEventListener('click', () => {
-    if(displayValue === '') {
-        return;
-    } else if(displayValue.length === 1) {
-        displayScreen.textContent = '';
-        displayValue = '';
-    } else {
-        let temp = displayValue;
-        temp = temp.substring(0, temp.length-1);
-        displayScreen.textContent = temp;
-        displayValue = temp;
-    }
-});
-
-window.addEventListener('keydown', pressNumber)
+button1.addEventListener('click', buttonOne);
+button2.addEventListener('click', buttonTwo);
+button3.addEventListener('click', buttonThree);
+button4.addEventListener('click', buttonFour);
+button5.addEventListener('click', buttonFive);
+button6.addEventListener('click', buttonSix);
+button7.addEventListener('click', buttonSeven);
+button8.addEventListener('click', buttonEight);
+button9.addEventListener('click', buttonNine);
+button0.addEventListener('click', buttonZero);
+buttonPlus.addEventListener('click', plusOperation);
+buttonMinus.addEventListener('click', minusOperation);
+buttonMultiply.addEventListener('click', multiplyOperation);
+buttonDivide.addEventListener('click', divideOperation);
+buttonEqual.addEventListener('click', equalOperation);
+buttonDelete.addEventListener('click', deleteOperation);
+buttonBack.addEventListener('click', backOperation);
+buttonDotted.addEventListener('click', dottedOperation);
+window.addEventListener('keydown', pressNumber);
 
 function pressNumber(e) {
     let key = e.keyCode;
     if(key === 49) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '1';
-        displayValue += '1';
-    } else if (key === 56) {
-        if(num1 === '' && displayValue === '') {
-            return;
-        } else if(num1 === '') {
-            num1 = Number(displayValue);
-            operation = '*';
-            displayValue = '';
-            displayScreen.textContent = '';
-        } else {
-            calculate();
-            operation = '*';
-            displayValue = '';
-            num2 = '';
-        }    
-    } else if (key === 187) {
-        if(num1 === '' && displayValue === '') {
-            return;
-        } else if(num1 === '') {
-            num1 = Number(displayValue);
-            operation = '+';
-            displayValue = '';
-            displayScreen.textContent = '';
-        } else {
-            calculate();
-            operation = '+';
-            displayValue = '';
-            num2 = '';
-        }    
+        buttonOne();
+    } else if (key === 77) {
+        multiplyOperation();
+    } else if (key === 80) {
+        plusOperation();
     } else if (key === 50) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '2';
-        displayValue += '2';
+       buttonTwo();
     } else if (key === 51) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '3';
-        displayValue += '3';
+        buttonThree();
     } else if (key === 52) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '4';
-        displayValue += '4';
+        buttonFour();
     } else if (key === 53) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '5';
-        displayValue += '5';
+        buttonFive();
     } else if (key === 54) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '6';
-        displayValue += '6';
+        buttonSix();
     } else if (key === 55) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '7';
-        displayValue += '7';
+        buttonSeven();
     } else if (key === 56) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '8';
-        displayValue += '8';
+        buttonEight();
     } else if (key === 57) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '9';
-        displayValue += '9';
+        buttonNine();
     } else if (key === 48) {
-        if(answer !== '' || erroMessage !== '') {
-            displayScreen.textContent = '';
-            answer = '';
-        }
-        displayScreen.textContent += '0';
-        displayValue += '0';
+        buttonZero();
     } else if (key === 8 ) {
-        if(displayValue === '') {
-            return;
-        } else if(displayValue.length === 1) {
-            displayScreen.textContent = '';
-            displayValue = '';
-        } else {
-            let temp = displayValue;
-            temp = temp.substring(0, temp.length-1);
-            displayScreen.textContent = temp;
-            displayValue = temp;
-        }
+        backOperation();
     } else if (key === 187 || key === 13) {
-        if(operation === '') {
-            return;
-        }
-        calculate();
-        num2 = '';
-        num1 = '';
-        operation = '';
-        displayValue = '';
+        equalOperation();
     } else if (key === 46) {
-        displayScreen.textContent = '';
-        num1 = '';
-        num2 = '';
-        operation = '';
-        displayValue = '';
-        answer = '';
+        deleteOperation();
     } else if (key === 189) {
-        if(num1 === '' && displayValue === '') {
-            return;
-        } else if(num1 === '') {
-            num1 = Number(displayValue);
-            operation = '-';
-            displayValue = '';
-            displayScreen.textContent = '';
-        } else {
-            calculate();
-            operation = '-';
-            displayValue = '';
-            num2 = '';
-        }    
+        minusOperation();
     } else if (key === 191) {
-        if(num1 === '' && displayValue === '') {
-            return;
-        } else if(num1 === '') {
-            num1 = Number(displayValue);
-            operation = '/';
-            displayValue = '';
-            displayScreen.textContent = '';
-        } else {
-            calculate();
-            operation = '/';
-            displayValue = '';
-            num2 = '';
-        }    
+        divideOperation();
     } else if (key === 190) {
-        for(let i = 0; i < displayValue.length; i++) {
-            if(displayValue[i] === '.') {
-                return;
-            }
-        }
-        displayScreen.textContent += ".";
-        displayValue += '.';
+        dottedOperation();
     } 
 }
 
